fd03c720cc1cbbf8776726c38083a2941daae411
MrReallyYo <mrreallyyo@gmail.com>
2023-03-06 11:04:04 +0100

    Add github distribution task.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     kotlin("jvm") version "1.8.0"
+    application
 }
 
 group = "com.github.mrreallyyo"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
@@ -23,7 +24,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.5")
     implementation("org.slf4j:slf4j-simple:2.0.6")
 
-    implementation("com.github.mrreallyyo:leitemfilter-api:1.1")
+    implementation("com.github.mrreallyyo:leitemfilter-api:1.2")
 
 
     testImplementation(kotlin("test"))
@@ -35,4 +36,28 @@ tasks.test {
 
 kotlin {
     jvmToolchain(8)
+}
+
+tasks.jar {
+    manifest.attributes["Main-Class"] = "com.github.mrreallyyo.leitemfilter.cli.MainKt"
+    manifest.attributes["Class-Path"] = configurations
+        .runtimeClasspath
+        .get()
+        .joinToString(separator = " ") { file ->
+            "libs/${file.name}"
+        }
+}
+
+distributions {
+    create("github") {
+        distributionBaseName.set("leitemfilter-cli")
+        contents {
+            from(tasks.jar)
+            into("libs") {
+                from(configurations.runtimeClasspath.get())
+            }
+            from("LICENSE")
+            from("README.md")
+        }
+    }
 }
